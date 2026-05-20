29dcdcd0188e1ea9607ec12b7321ae40893cb7b5
ShahJabir <shahjabirofficial@gmail.com>
2026-05-20 23:34:49 +0600

    filtering: add SQL anti join and cross join examples

--- postgres/src/join.sql
+++ postgres/src/join.sql
@@ -23,3 +23,43 @@ SELECT c.name, o.product, o.amount
 FROM customers c
 FULL JOIN orders o
 ON c.id=o.customer_id;
+
+/*
+SQL Anti Join
+*/
+
+SELECT c.name
+FROM customers c
+LEFT JOIN orders o
+ON c.id = o.customer_id
+WHERE o.customer_id ISNULL;
+
+SELECT o.customer_id
+FROM customers c
+RIGHT JOIN orders o
+ON c.id = o.customer_id
+WHERE c.id ISNULL;
+
+SELECT *
+FROM customers c
+FuLL JOIN orders o
+ON c.id = o.customer_id
+WHERE c.id ISNULL or o.customer_id ISNULL;
+
+-- Task: Get All customers who have something purchase without inner join
+
+SELECT c.name, SUM(o.amount) AS total_cost
+FROM customers c
+RIGHT JOIN orders o
+ON c.id = o.customer_id
+WHERE c.id IS NOT NULL
+GROUP BY c.name
+ORDER BY total_cost DESC;
+
+/*
+CROSS JOIN
+*/
+
+SELECT *
+FROM customers
+CROSS JOIN orders;
