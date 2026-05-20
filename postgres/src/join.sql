3669adc1a7227e66762517bee6068dd106df90f4
ShahJabir <shahjabirofficial@gmail.com>
2026-05-20 23:48:10 +0600

    filtering: add SQL self join example

--- postgres/src/join.sql
+++ postgres/src/join.sql
@@ -63,3 +63,14 @@ CROSS JOIN
 SELECT *
 FROM customers
 CROSS JOIN orders;
+
+/*
+SELF JOIN
+*/
+
+-- Find each employee and their manager's name
+SELECT
+    e.name        AS employee,
+    m.name        AS manager
+FROM employees e
+LEFT JOIN employees m ON e.manager_id = m.id;
