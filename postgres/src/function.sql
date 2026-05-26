a37f8f28f26449a634d100f5d498468e34d1920b
ShahJabir <shahjabirofficial@gmail.com>
2026-05-26 22:07:14 +0600

    function: add SQL String Function

---
+++ postgres/src/function.sql
@@ -0,0 +1,74 @@
+/*
+String Function - Manipulation - Concat
+*/
+
+SELECT first_name, country,
+CONCAT(first_name, '-', country)
+FROM customers;
+
+/*
+String Function - Manipulation - Upper
+*/
+
+SELECT first_name,
+UPPER(first_name)
+FROM customers;
+
+/*
+String Function - Manipulation - Lower
+*/
+
+SELECT first_name,
+LOWER(first_name)
+FROM customers;
+
+/*
+String Function - Manipulation - Trim
+& Calculation - Length
+*/
+
+SELECT first_name,
+LENGTH(first_name) AS before_trim,
+LENGTH(TRIM(first_name)) AS after_trim
+FROM customers
+WHERE LENGTH(first_name) != LENGTH(TRIM(first_name));
+
+/*
+String Function - Manipulation - Replace
+*/
+
+SELECT order_date,
+    REPLACE(order_date::TEXT, '-', '/') AS formatted_date
+FROM orders;
+
+/*
+String Function - Manipulation - TO_CHAR
+*/
+
+SELECT order_date,
+       TO_CHAR(order_date, 'YYYY/MM/DD') AS formatted_date
+FROM orders;
+
+/*
+String Function - String Extraction - Left
+*/
+
+SELECT first_name,
+       LEFT(TRIM(first_name), 2) AS name_2_char
+FROM customers;
+
+/*
+String Function - String Extraction - Right
+*/
+
+SELECT first_name,
+       RIGHT(TRIM(first_name), 2) AS name_2_char
+FROM customers;
+
+/*
+String Function - String Extraction - SubString
+*/
+
+SELECT first_name,
+       SUBSTRING(TRIM(first_name), 2, LENGTH(first_name)) AS sub_name
+FROM customers;
